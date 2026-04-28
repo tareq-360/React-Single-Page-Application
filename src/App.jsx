9ba1aa7446ba83e4e2357bbbce5763bd3692f0a7
tareq-360 <mdtareqhossain2002@gmail.com>
2026-04-28 18:42:58 +0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Component/Navbar/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=" text-red-500">Hello React</h1>
+      <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
